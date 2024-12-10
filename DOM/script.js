19177b4b2db5a4e88683627db328d6f92f1577a2
dasamaf <haltriescode@gmail.com>
2024-12-10 23:14:59 +0700

    learn DOM

---
+++ DOM/script.js
@@ -0,0 +1,11 @@
+// Selecting Elements
+// By ID
+const selectId = document.getElementById("button-1");
+selectId.textContent = "Button";
+
+// By Class
+const selectClass = document.getElementsByClassName("test");
+
+// By TagName
+const selectTag = document.getElementsByTagName("div");
+
